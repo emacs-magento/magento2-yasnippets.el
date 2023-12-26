ba5561eda79eef2f6f2f6ce580846a68e5b68e57
Piotr Kwiecinski <piotr.kwiecinski@codemanufacture.com>
2023-12-26 18:13:38 +0100

    Initial commit

---
+++ magento2-yasnippets.el
@@ -0,0 +1,48 @@
+;;; magento2-yasnippets.el --- Collection of Magento 2 snippets -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Piotr Kwiecinski
+;;
+;; Version: 0.1.0
+;; Package-Requires: ((yasnippet "0.8.0"))
+;; Keywords: snippets
+;; Homepage: https://github.com/emacs-magento/magento2-yasnippets.el
+;;
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; Magento 2 snippet collection for yasnippet package.
+;;
+;;; Code:
+
+(require 'yasnippet)
+
+(defvar magento2-yasnippets-dir
+  (file-name-directory (or load-file-name (buffer-file-name))))
+
+;;;###autoload
+(defun magento2-yasnippets-initialize ()
+  "Load `magento2-yasnippets' snippets directory."
+  (let ((snippet-dir (expand-file-name "snippets" magento2-yasnippets-dir)))
+    (when (boundp 'yas-snippet-dirs)
+      (add-to-list 'yas-snippet-dirs 'magento2-yasnippets-dir t))
+    (yas-load-directory magento2-yasnippets-dir t)))
+
+;;;###autoload
+(eval-after-load 'yasnippet (magento2-yasnippets-initialize))
+
+(provide 'magento2-yasnippets)
+;;; magento2-yasnippets.el ends here
